e0136ca8678f4751a928bc3b74c8b35bd98363ab
HOIL <hoil20707@gmail.com>
2021-01-31 21:58:02 +0900

    초기 커밋.

---
+++ FileManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyEnglishWord
+{
+    [Serializable]
+    public class WordData
+    {
+        public string word;
+        public List<string> date;
+        public int cnt;
+    }
+
+    public class FileMng
+    {
+        public static List<WordData> wordDatas;
+        public static List<string> words = new List<string>();
+        public static string dataPath = @"data.dat";
+        public static string wordsPath = @"words.txt";
+        public static void LoadData()
+        {
+            FileInfo fileInfo = new FileInfo(dataPath);
+            if (fileInfo.Exists)
+            {
+                using (FileStream fs = new FileStream(dataPath, FileMode.OpenOrCreate))
+                {
+                    BinaryFormatter binary = new BinaryFormatter();
+                    try
+                    {
+                        wordDatas = (List<WordData>)binary.Deserialize(fs);
+                    }
+                    catch(Exception ex) 
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+
+        public static void SaveData()
+        {
+            using (FileStream fs = new FileStream(dataPath, FileMode.Create, FileAccess.Write))
+            {
+                BinaryFormatter binary = new BinaryFormatter();
+                binary.Serialize(fs, wordDatas);
+            }
+        }
+
+        public static void LoadWords()
+        {
+            string wordList = File.ReadAllText(wordsPath);
+
+            string word = "";
+            int isEnglishWord = 0;
+            for (int i = 0; i < wordList.Length; i++)
+            {
+                if (wordList[i] >= 'a' && wordList[i] <= 'z' || (wordList[i] == ' ' && word.Length != 0))
+                {
+                    if(EndSpaceCount(word) >= 2) // 공백이 2개 이상일 때 추가
+                    {
+                        AddWord(word);
+                        word = "";
+                        continue;
+                    }
+                    isEnglishWord = 0;
+                    word += wordList[i];
+                }
+                else
+                {
+                    isEnglishWord++;
+                    if (isEnglishWord >= 2) // 영어가 아닌 다른 숫자가 2번 이상 나왔을 때
+                    {
+                        AddWord(word);      // 알파벳이 하나라도 포함되어 있을 때 추가
+                        word = "";
+                    }
+                }
+            }
+            
+        }
+
+        //-----------------------------------------
+
+        static int EndSpaceCount(string word)
+        {
+            int cnt = 0;
+            for(int i = word.Length -1; i >= 0; i--)
+            {
+                if (word[i] == ' ') cnt++;
+                else break;
+            }
+            return cnt;
+        }
+
+        static void AddWord(string word)
+        {
+            if (word.Length != 0)
+            {
+                word = RemoveEndSpace(word);
+                words.Add(word);
+            }
+        }
+
+
+        static string RemoveEndSpace(string str)
+        {
+            int i = str.Length - 1;
+            while (i >= 0)
+            {
+                if (str[i] == ' ')
+                {
+                    str = str.Remove(i);
+                    i = str.Length - 1;
+                }
+                else break;
+
+            }
+            return str;
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyEnglishWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FileMng.LoadWords();
+            FileMng.LoadData();
+
+            //foreach (var item in FileMng.words)
+            //{
+            //    listBox1.Items.Add(item);
+            //}
+            
+        }
+    }
+}
